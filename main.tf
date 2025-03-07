ac6250f3fc0987399252e6b3a47edf395ec2e602
Reed Kraft-Murphy <reed.kraftmurphy@hashicorp.com>
2025-03-07 09:12:32 -0800

    Remove install test

--- main.tf
+++ main.tf
@@ -33,14 +33,3 @@ resource "terraform_data" "aws_cli" {
     command = "aws --version"
   }
 }
-
-resource "terraform_data" "install_aws_cli" {
-  triggers_replace = [
-    uuid()
-  ]
-  provisioner "local-exec" {
-    command = "PIP_BREAK_SYSTEM_PACKAGES=1 pip install 'awscli ~= 1.30.0'; aws --version"
-  }
-
-  depends_on = [terraform_data.aws_cli]
-}
